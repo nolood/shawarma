8162aca2843df0cd0baa645bce842b42c5301ecd
nolood <a9514548799@gmail.com>
2023-04-26 14:13:20 +0300

    slider

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const burger = document.querySelector('.navbar__burger')
 const nabarList = document.querySelector('.navbar__list')
 const catalogTabs = document.querySelectorAll('.catalog__item')
 const catalogTabsContent = document.querySelectorAll('.catalog__content')
+const slider = document.querySelector('.reviews__list')
+const nextSlide = document.getElementById('next')
+const prevSlide = document.getElementById('prev')
+let offset = 0;
+
+nextSlide.addEventListener('click', () => {
+  offset += 1200;
+  if (offset > 3600) {
+    offset = 0
+  }
+  slider.style.left = -offset + 'px'
+})
+
+prevSlide.addEventListener('click', () => {
+  offset -= 1200;
+  if (offset < 0) {
+    offset = 3600
+  }
+  slider.style.left = -offset + 'px'
+})
+
 
 
 catalogTabs.forEach(tab => {
